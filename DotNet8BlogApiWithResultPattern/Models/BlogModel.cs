4aca8d21bdd07cb4fddee9d9229c97847f89bfa2
minthukha <minthukha1239788@gmail.com>
2024-06-29 23:26:48 +0630

    [ Added ] - BlogModel

---
+++ DotNet8BlogApiWithResultPattern/Models/BlogModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet8BlogApiWithResultPattern.Models
+{
+    public class BlogModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+    }
+}
