1a64555aeaccb7ba9bcda4d97b28b33b65166911
minthukha <minthukha1239788@gmail.com>
2024-06-29 23:27:00 +0630

    [ InProgress ] - BlogController

---
+++ DotNet8BlogApiWithResultPattern/Controllers/BlogController.cs
@@ -0,0 +1,24 @@
+﻿using DotNet8BlogApiWithResultPattern.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8BlogApiWithResultPattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly IBlogService _blogService;
+
+        public BlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetBlogsAsync()
+        {
+            var result = await _blogService.GetBlogsAsync();
+            return result.IsSuccess ? Ok(result) : BadRequest(result.Error);
+        }
+    }
+}
