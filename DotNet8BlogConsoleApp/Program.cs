1522d67374462acc43cd4a813366be975a3c7091
minthukha <minthukha1239788@gmail.com>
2024-06-29 21:31:36 +0630

    Add project files.

---
+++ DotNet8BlogConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet8BlogConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
