ecce17061886174ca9827a3c577c8eda97ca7465
minthukha <minthukha1239788@gmail.com>
2024-06-29 23:26:14 +0630

    [ InProgress ] - BlogService

---
+++ DotNet8BlogApiWithResultPattern/Services/BlogService.cs
@@ -0,0 +1,22 @@
+﻿using DotNet8BlogApiWithResultPattern.BlogSamplesList;
+using DotNet8BlogApiWithResultPattern.Models;
+using DotNet8BlogApiWithResultPattern.Shared;
+
+namespace DotNet8BlogApiWithResultPattern.Services
+{
+    public class BlogService : IBlogService
+    {
+        public Task<Result<IEnumerable<BlogModel>>> GetBlogsAsync()
+        {
+            //var blog = BlogSimpleList.GetBlogs();
+            //if(blog == null)
+            //{
+            //    return Task.FromResult(Result.Failure<BlogModel>("Blogs are not found"));
+            //}
+            //return Task.FromResult(Result.Success(blog));
+
+            var blog = BlogSimpleList.GetBlogs();
+            return Task.FromResult(Result.Success(blog));
+        }
+    }
+}
---
+++ DotNet8BlogApiWithResultPattern/Services/IBlogService.cs
@@ -0,0 +1,10 @@
+﻿using DotNet8BlogApiWithResultPattern.Models;
+using DotNet8BlogApiWithResultPattern.Shared;
+
+namespace DotNet8BlogApiWithResultPattern.Services
+{
+    public interface IBlogService
+    {
+        Task<Result<IEnumerable<BlogModel>>> GetBlogsAsync();
+    }
+}
