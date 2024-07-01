49ea0517f6c511f1bf359586e2ead3e4868f2409
minthukha <minthukha1239788@gmail.com>
2024-07-01 10:29:07 +0630

    [ Added ] - Shared Project

---
+++ Shared/Models/BlogModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shared.Models;
+
+[Table("Tbl_blog")]
+
+public class BlogModel
+{
+    [Key]
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+}
